117d754db2273f45a79aa240450526a87521615e
nadapapa <nadapapa@gmail.com>
2016-12-04 20:24:26 +0100

    add models

---
+++ ng2-admin/app/models/feedback.model.ts
@@ -0,0 +1,19 @@
+export class Feedback {
+
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+  reporter_name: string;
+  reporter_email: string;
+  browser: string;
+  platform: string;
+  user_agent: string;
+  screen_resolution: string;
+  cookie_enabled: boolean;
+  project_id: number
+  ext_user_id: number;
+  createdAt: string;
+  updatedAt: string;
+
+}
---
+++ ng2-admin/app/models/project.model.ts
@@ -0,0 +1,10 @@
+export class Project {
+
+  id: number;
+  ext_id: number;
+  name: string;
+  issue_tracker: string;
+  createdAt: string;
+  updatedAt: string;
+
+}
