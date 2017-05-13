85dfa44abdc6f51c47c0a3016564f7c76240fd94
nadapapa <nadapapa@gmail.com>
2017-05-13 09:35:34 +0200

    polish the install process

--- dply-script.sh
+++ dply-script.sh
@@ -10,27 +10,27 @@ mkswap /root/myswapfile;
 swapon /root/myswapfile;
 
 
-echo "--- initial apt update";
+echo -e "\n--- initial apt update";
 apt-get update;
 apt-get -y upgrade;
 
-echo "--- install curl, git, wget";
+echo -e "\n--- install curl, git, wget";
 apt-get install -y curl git wget;
 
-echo "--- add dotdeb";
+echo -e "\n--- add dotdeb";
 echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list.d/dotdeb.org.list && \
 echo "deb-src http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list.d/dotdeb.org.list && \
 wget -O- http://www.dotdeb.org/dotdeb.gpg | apt-key add -;
 
-echo "--- set mysql password";
+echo -e "\n--- set mysql password";
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections;
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections;
 
-echo "--- install nodejs";
+echo -e "\n--- install nodejs";
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh;
 bash nodesource_setup.sh;
 
-echo "--- main apt install: php, nginx, mysql, nodejs";
+echo -e "\n--- main apt install: php, nginx, mysql, nodejs";
 apt-get update;
 apt-get install -y php7.0-common \
                    php7.0-cli \
@@ -49,19 +49,18 @@ apt-get install -y php7.0-common \
                    mysql-server \
                    nodejs;
 
-echo "--- create db";
+echo -e "\n--- create db";
 mysql --execute="create database remarker" --password=root;
 
-echo "--- create directory";
-mkdir -p /var/www;
-cd /var/www;
-rm -rf *;
+echo -e "\n--- create directory";
+mkdir -p /var/www/remarker;
+cd /var/www/remarker;
 
-echo "--- clone repo";
+echo -e "\n--- clone repo";
 git clone https://github.com/bigfish-hu/remarker.git .;
 cp .env.example .env;
 
-echo "--- copy files";
+echo -e "\n--- copy files";
 cp Docker/web/conf/php/000-php-settings.ini /etc/php/7.0/mods-available/000-php-settings.ini;
 cp Docker/web/conf/php/000-php-settings.ini /etc/php/7.0/cli/conf.d/000-php-settings.ini;
 cp Docker/web/conf/php/000-php-settings.ini /etc/php/7.0/fpm/conf.d/000-php-settings.ini;
@@ -74,19 +73,23 @@ rm -Rf /etc/nginx/sites-enabled/*;
 cp Docker/web/conf/nginx/nginx-site.conf /etc/nginx/sites-available/default.conf;
 ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf;
 
-echo "--- composer install";
+echo -e "\n--- composer install";
 curl -sS https://getcomposer.org/installer | sudo php
 php composer.phar install;
 
-echo "--- run artisan commands";
+echo -e "\n--- copy .env.dply to .env ---";
+rm -f .env
+cp .env.dply .env
+
+echo -e "\n--- run artisan commands";
 php artisan migrate;
 php artisan db:seed;
 
-echo "--- npm install, build";
+echo -e "\n--- npm install, build";
 cd admin && npm install;
 ./node_modules/.bin/ng build --prod
 
-chown -R www-data:www-data /var/www;
+chown -R www-data:www-data /var/www/remarker;
 service nginx restart;
 
 echo "====== END: $(date +"%T")";
--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+echo -e "====== START: $(date +"%T")";
+echo -e "\n--- docker-compose build ---";
 docker-compose build &&
+echo -e "\n--- docker-compose up -d ---";
 docker-compose up -d &&
-docker exec -it remarker_web_1 su - composer -c "cd /var/www/remarker && composer install" &&
-docker exec -it remarker_web_1 php artisan migrate &&
-docker exec -it remarker_web_1 php artisan db:seed &&
-docker exec -it remarker_web_1 su - composer -c "cd /var/www/remarker && php artisan vendor:publish" &&
+echo -e "\n--- composer install ---";
+docker exec remarker_web_1 su - composer -c "cd /var/www/remarker && composer install" &&
+echo -e "\n--- change owner of storage/ and bootstrap/cache ---";
+docker exec remarker_web_1 chown -R www-data storage/ bootstrap/cache &&
+echo -e "\n--- migrate db ---";
+docker exec remarker_web_1 php artisan migrate &&
+echo -e "\n--- seed db ---";
+docker exec remarker_web_1 php artisan db:seed &&
+echo -e "\n--- publish vendor files ---";
+docker exec remarker_web_1 su - composer -c "cd /var/www/remarker && php artisan vendor:publish" &&
+echo -e "\n--- admin build start ---";
 cd admin &&
+echo -e "\n--- npm install ---";
 npm install &&
+echo -e "\n--- build admin ---";
 npm run build &&
 cd ..
